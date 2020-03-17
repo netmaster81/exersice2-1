70efb8ae0f3514d2056345d5f62955a6b1e43d9c
ahmad <hafar.ahmad@gmail.com>
2020-03-17 16:40:52 +0100

    final project

--- exersice2-1/Program.cs
+++ exersice2-1/Program.cs
@@ -11,24 +11,12 @@ namespace exersice2_1
         static void Main(string[] args)
         {
 
-            //CinemaTicket CC= new CinemaTicket();
-            //CC.Main();
-            //RepeatTen CC = new RepeatTen(); ;
-            //CC.main();
-            //ThirdWord CC = new ThirdWord();
-            //CC.Main();
+            
             Mainmenu MM = new Mainmenu();
             MM.TaskPrintMenu();
             MM.Menu();
-            Console.ReadLine();
-            Console.ReadLine();
-            Console.ReadLine();
-            Console.ReadLine();
-            Console.ReadLine();
-            Console.Write("Hej");
-            //Console.ReadLine();
-            //Console.ReadLine();
-            //Console.ReadLine();
+            
+            
         }
     }
 }

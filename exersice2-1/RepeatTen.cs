d259b5dd7bd781d08f60147e3f7ad9e8fa3e3aab
netmaster81 <61751225+netmaster81@users.noreply.github.com>
2020-03-17 14:18:54 +0100

    Update RepeatTen.cs

--- exersice2-1/RepeatTen.cs
+++ exersice2-1/RepeatTen.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace exersice2_1
 {
-    class RepeatTen
+    public class RepeatTen
     {
+        private string Text;// input Text
+        public RepeatTen()
+        {
+
+        }
+
+        public void Main()
+        {
+            Console.WriteLine("This software repeat to you the text you intered ten times");
+            Console.WriteLine("Please Enter Your Text");
+            Text = Console.ReadLine();
+            string repeat="";
+            for (int i=0;i<10;i++)
+            {
+                repeat = repeat + (i + 1) + "." + Text + ", ";// repeat Text ten times with numbering 
+            }
+            repeat = repeat.Remove(repeat.Length-2);// delete the last charcter ',' 
+
+            Console.WriteLine($" {repeat}");// print the result
+            
+        }
+     
     }
 }
+
